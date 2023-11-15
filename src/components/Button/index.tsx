29affc2dc978d59c6d88165fb30c4e4c6f41f527
Gabriel Salvador <102549645+ogabrielsalvador@users.noreply.github.com>
2023-11-15 17:19:03 -0300

    refactor(Button): altera customização e adiciona modo loading

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,36 +2,42 @@ import { ComponentPropsWithoutRef, forwardRef } from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
+import { Skeleton } from '../Skeleton'
+
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex whitespace-nowrap justify-center items-center rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       color: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+        primary:
+          'border-blue-900 outline-blue-900 bg-blue-700 text-white hover:bg-blue-800 hover:border-blue-950 hover:outline-blue-950 disabled:bg-blue-100 disabled:text-blue-600 disabled:outline-blue-200',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+          'border-slate-300 outline-slate-300 bg-slate-100 text-slate-700 hover:border-slate-400 hover:outline-slate-400 hover:bg-slate-200 disabled:bg-slate-50 disabled:text-slate-600 disabled:outline-slate-200',
+        tertiary:
+          'border-slate-300 outline-slate-300 bg-white text-slate-700 hover:border-slate-400 hover:outline-slate-400 disabled:outline-slate-50',
+        quaternary:
+          'border-blue-600 outline-blue-600 bg-white text-blue-700 hover:bg-blue-100 hover:border-blue-800 hover:outline-blue-800 disabled:outline-slate-50',
+        success:
+          'border-teal-900 outline-teal-900 bg-teal-700 text-white hover:bg-teal-800 hover:outline-teal-950 hover:border-teal-950 disabled:bg-teal-100 disabled:text-teal-600 disabled:outline-teal-200',
+        danger:
+          'border-pink-900 outline-pink-900 bg-pink-700 text-white hover:bg-pink-800 hover:border-pink-950 disabled:text-pink-600 hover:outline-pink-950 disabled:bg-pink-100 disabled:outline-pink-200',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        xs: 'text-xs py-0.5 xl:py-1 2xl:py-1.5 px-1 xl:px-1.5 2xl:px-2',
+        sm: 'text-sm py-1 xl:py-1.5 2xl:py-2 px-2.5 xl:px-3 2xl:px-3.5',
+        md: 'text-base py-1.5 xl:py-2 2xl:py-2.5 px-3.5 xl:px-4 2xl:px-5',
+        lg: 'text-xl py-2 xl:py-2.5 2xl:py-3 px-4 xl:px-5 2xl:px-6',
+        xl: 'text-3xl py-2.5 xl:py-3 2xl:py-3.5 px-5 xl:px-6 2xl:px-7',
       },
       pill: {
         true: 'rounded-full',
       },
     },
     defaultVariants: {
-      color: 'default',
-      size: 'default',
+      color: 'secondary',
+      size: 'md',
       pill: false,
     },
   },
@@ -39,19 +45,44 @@ const buttonVariants = cva(
 
 type ButtonProps = {
   asChild?: boolean
+  loading?: boolean
 } & ComponentPropsWithoutRef<'button'> &
   VariantProps<typeof buttonVariants>
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ asChild = false, color, size, pill, className, ...props }, ref) => {
+  (
+    {
+      asChild = false,
+      loading = false,
+      color,
+      size,
+      pill,
+      className,
+      children,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : 'button'
 
     return (
-      <Comp
-        className={cn(buttonVariants({ color, size, pill, className }))}
-        ref={ref}
-        {...props}
-      />
+      <>
+        {loading ? (
+          <Skeleton
+            className={cn(buttonVariants({ color, size, pill, className }))}
+          >
+            {children}
+          </Skeleton>
+        ) : (
+          <Comp
+            className={cn(buttonVariants({ color, size, pill, className }))}
+            ref={ref}
+            {...props}
+          >
+            {children}
+          </Comp>
+        )}
+      </>
     )
   },
 )
